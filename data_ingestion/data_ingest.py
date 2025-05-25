883f10da1f12a9bd73ef82fe0574901c1cfe9794
Manas Agarwal <manasmrt10@gmail.com>
2025-05-25 18:49:23 +0530

    data_transformation_completed

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -2,5 +2,14 @@ from langchain_astradb import AstraDBVectorStore
 from dotenv import load_dotenv
 import pandas
 import os
-from data_ingestion.data_transform import Data_Converter
+from data_ingestion.data_transform import DataConverter
 
+load_dotenv()
+
+class IngestData:
+    
+    def __init__(self):
+        pass
+    
+    def data_ingestion(self):
+        pass
