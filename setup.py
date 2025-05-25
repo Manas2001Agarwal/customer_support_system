2388ac77f7820c4c3da18529a83b7f0da7b4b9e8
Manas Agarwal <manasmrt10@gmail.com>
2025-05-25 17:11:18 +0530

    code_updated

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="e-commerce-bot",
+      version='0.0.1',
+      author = "Manas Agarwal",
+      author_email="manasmrt10@gmail.com",
+      packages=find_packages(),
+      install_requires = ['langchain','langchain_astradb'])
